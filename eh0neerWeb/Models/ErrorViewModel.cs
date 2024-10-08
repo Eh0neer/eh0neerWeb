ff075616ec1be52dd5bea78eddda531443d4b741
Косарев Андрей Иванович <aik@2i.tusur.ru>
2024-10-08 15:32:08 +0700

    [FIX]: Adding files not covered by .gitignore

---
+++ eh0neerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eh0neerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
